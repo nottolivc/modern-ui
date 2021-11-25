74f62d58f2269925da9fd43ff7f472a3addd463b
Andrew S <nottolivc@gmail.com>
2021-11-25 14:42:05 -0800

    temp add link to markets

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Learn How to Build the Future of the Web with GPT-3 OpenAI</h1>
-      <p>Leverage the largest AI library ever built today as a developer, trader, or entrepreneur.</p>
-
+      <p>Leverage the largest AI library ever built today as a developer, trader, or entrepreneur.
+      <a href="https://cryptograph-gpt-3.netlify.app"><br /><br /><h2>Click Here to See Realtime Market Data (Traders)</h2></a></p>
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
         <button type="button">Get Started</button>
