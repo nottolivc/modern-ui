e5d0f92bf81f9f6d485862b4239a929ddfe25a4e
andrew s <nottolivc@gmail.com>
2021-12-10 02:08:48 -0800

    add staging for text modification and ab testing services

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => (
     </div>
 
     <div className="gpt3__footer-links">
-      <div className="gpt3__footer-links_logo">
+      {/* <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
         <p>Open Source   https://openai.com/blog/gpt-3-apps<br /></p>
-      </div>
+      </div> */}
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
         <p>Home</p>
