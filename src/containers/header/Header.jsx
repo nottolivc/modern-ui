7c4cc829821f99a054948b76fdbb3cfbe3e6b195
andrew s <nottolivc@gmail.com>
2021-12-13 20:35:12 -0800

    add build of basic api for chat and email list

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,8 @@ import CustomChatbot from '../../CustomChatbot';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">&Sigma; SIGMA Source Tech: Open AI Powered FinTech Development</h1>
+      <h1 className="gradient__text">&Sigma;</h1><h1>Sigma Sourcing</h1>
+      <h2 className="gradient__text">Power Your Business with AI, Data & Cloud Technology</h2>
       <p>Tech consulting and candidate sourcing for top technology and financial firms. 
         Leverage bleeding edge tech for building the future of your company on the web and it's developers. 
         Learn to use/develop tools with the largest AI & ML library ever built today as a engineering team, hiring manager, 
@@ -22,7 +23,7 @@ const Header = () => (
         <p style={{'color': '#444'}}>Source from over 100,000 data points in the our propietary database</p>
       </div>
       <div className="chat">
-        <p style={{color: '#444'}}>Chatbot for basic info</p>
+        <p style={{color: '#444'}}>Chatbot </p>
         <CustomChatbot />
       </div>
     </div>
