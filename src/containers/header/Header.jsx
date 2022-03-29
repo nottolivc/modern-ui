47d06905d58d9b5e9540f377decd00cb71476c21
andrew s <nottolivc@gmail.com>
2022-03-29 01:04:48 -0700

    add build of basic api for chat and email list

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-// import people from '../../assets/people.png';
-// import ai from '../../assets/ai.png';
 import './header.css';
-//import CustomChatbot from '../../CustomChatbot';
 
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">&Sigma;</h1><h1>Sigma Sourcing</h1>
-      <h2 className="gradient__text">Leverage your productivity with deep learning and AI</h2>
+      <h2 className="gradient__text">Leverage your productivity</h2>
       <p>Tech consulting and candidate sourcing for top technology and financial firms. 
         Leverage bleeding edge tech for building the future of your company on the web and it's developers. 
         Learn to use/develop tools with the largest AI & ML library ever built today as a engineering team, manager, 
